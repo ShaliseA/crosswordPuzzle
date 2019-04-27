3bd517b1d3defb680484a5720524c537e09cfe44
ShaliseA <shaliseayromloo@gmail.com>
2019-04-27 10:40:01 -0500

    Added a payment view controller.

---
+++ paymentViewController.swift
@@ -0,0 +1,22 @@
+//
+//  paymentViewController.swift
+//  crosswordPuzzle
+//
+//  Created by Shalise Ayromloo on 4/27/19.
+//  Copyright © 2019 Shalise Ayromloo. All rights reserved.
+//
+
+import UIKit
+
+class paymentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
